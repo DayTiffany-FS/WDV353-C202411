3a4a68be21d81ca4b0060993285f16539e6e4cc1
DayTiffany-FS <TKDay@student.fullsail.edu>
2024-10-30 20:20:08 -0400

    error handling

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ##10.30.24 - Added branch for Module 1
    -- corrected code for Promise and .then() chain
    -- added error handling
+   -- setup for class assignment
+   -- error handling
--- app/app.js
+++ app/app.js
@@ -13,4 +13,17 @@ app.get('/', (req, res) => {
 `localhost:300/api`;
 app.use('/api', router);
 
+app.use((req, res, next) => {
+    const error = new Error('Not kosher');
+    error.status = 404;
+    next(error);
+});
+
+app.use((err, req, res, next) => {
+    console.log('ERROR >>>', err);
+    res
+        .status(err.status || 500)
+        .json({ message: err.message, status: err.status });
+});
+
 module.exports = app;
