7393fe5b26373bf1d0e5c686d8aef9e0f1839116
DayTiffany-FS <TKDay@student.fullsail.edu>
2024-11-03 18:52:19 -0500

    HTTP request and Response

--- server.js
+++ server.js
@@ -2,6 +2,29 @@ const http = require('http');
 require('dotenv').config();
 const app = require('./app/app');
 
-http.createServer(app).listen(process.env.PORT,()=>{
-    console.log('server is running on port', process.env.PORT);
+//http.createServer(app).listen(process.env.PORT,()=>{
+//    console.log('server is running on port', process.env.PORT);
+//});
+
+const todos = [
+    {id: 1, task: "Task One"},
+    {id: 2, task: "Task Two"},
+    {id: 3, task: "Task Three"},
+];
+
+const server = http.createServer((req,res) => {
+    res.setHeader("Content-Type", "application/json");
+    res.end(
+        JSON.stringify({
+            success: true,
+            method: req.method,
+            data: todos,
+        })
+    );
+});
+
+const PORT = 3000;
+
+server.listen(PORT, () => {
+    console.log(`listening on ${PORT}`);
 });
