108456c6d94754fa23eab31d4c69e72b2ed0b32f
DayTiffany-FS <TKDay@student.fullsail.edu>
2024-11-03 19:45:43 -0500

    1.4 Let's Code NodeJS

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,22 +1,40 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-`localhost:3000/api`;
-router.get('/', (req, res) => {
-    res.status(200).json({message: 'From the API'});
+// localhost:3000/api
+router.get("/", (req, res) => {
+    res.status(200).json({
+        message: "GET to API ",
+        metadata: {
+            hostname: req.hostname,
+            method: req.method
+        },
+    });
 });
 
-`localhost:3000/api/:city`;
-router.get('/:city', (req, res) => {
-    const city = req.params.city;
-    console.log('params >>>', city);
-    res.status(200).json({message: 'From the API'});
+// localhost:3000/api/<anything that follows>
+router.get("/:id", (req, res) => {
+    const { id } = req.params;
+    res.status(200).json({
+        message: "GET by Id for /api",
+        metadata: {
+            hostname: req.hostname,
+            method: req.method,
+            id,
+        },
+    });
 });
 
-router.post('/', (req, res) => {
-    console.log('Request body >>>', req.body);
-    const {data} = req.body;
-    res.status(200).json({message: 'We got the request', data});
+router.post("/", (req, res) => {
+    const { data } = req.body;
+    res.status(200).json({
+        message: "POST to /api",
+        data,
+        metadata: {
+            hostname: req.hostname,
+            method: req.method,
+        },
+    });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,26 +1,10 @@
 const http = require('http');
 require('dotenv').config();
-const app = require('./app/app');
+const app = require("./app");
+const router = require("./app/routes");
 
-const todos = [
-    {id: 1, task: "Task One"},
-    {id: 2, task: "Task Two"},
-    {id: 3, task: "Task Three"},
-];
+const server = http.createServer(app);
 
-const server = http.createServer((req,res) => {
-    res.setHeader("Content-Type", "application/json");
-    res.end(
-        JSON.stringify({
-            success: true,
-            method: req.method,
-            data: todos,
-        })
-    );
-});
-
-const PORT = 3000;
-
-server.listen(PORT, () => {
-    console.log(`listening on ${PORT}`);
+server.listen(process.env.PORT, () => {
+    console.log(`listening on ${process.env.PORT}`);
 });
