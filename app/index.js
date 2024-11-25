3c59079a49ce06e64fc7ea3d811b3055529e65ec
DayTiffany-FS <TKDay@student.fullsail.edu>
2024-11-24 21:13:22 -0500

    link front and back

--- app/index.js
+++ app/index.js
@@ -1,10 +1,20 @@
 const express = require("express");
+const cors = require('cors');
 const morgan = require("morgan");
 const app = express();
 const routeHandler = require("./routes");
 
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(cors());
+
+app.get('/api/posts', (req, res) => {
+    res.json([{id: 1, title: 'First Band' }, {id: 2, title: 'Second Band' }]);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
 
 app.get("/", (req,res) => {
     res.status(200).json({message: "API is running", success: true})
